f7ebfe8d5120a330ae6d25c4877a8bdb04e7d341
domifar <dominik.faritsch@sbg.at>
2024-09-13 11:59:00 +0200

    Grundprogramm

---
+++ Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Design_Pattern
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person() { }
+        public Person(Person person)
+        {
+            Name = person.Name;
+            Age = person.Age;
+        }
+
+        public Person Clone()
+        {
+            return new Person(this);
+        }
+
+        public override string ToString()
+        {
+            return $"\nName: {Name} \nAge: {Age}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Design_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person { Name = "Dominik", Age = 17 };
+
+            Person person2 = person1.Clone();
+
+            Console.WriteLine("Klonen" + person1);
+            Console.WriteLine("\nKlon" + person2);
+
+            person2.Name = "Amar";
+            person2.Age = 17;
+
+            Console.WriteLine("\nNach Änderung des Klons");
+            Console.WriteLine("\nOriginal" + person1);
+            Console.WriteLine("\nKlon" + person2);
+        }
+    }
+}
